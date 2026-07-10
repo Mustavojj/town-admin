93ec1491c64fde9e89e6abc73c46008c0ec5bd50
Mustavojj <mostavojj@gmail.com>
2026-07-10 12:48:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'admin.html'));
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server running on port ${PORT}`);
 });
